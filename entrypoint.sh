212be6e85df128e65523911b683b870afde64ee5
Roman Bykovskiy <8026787@gmail.com>
2023-04-07 01:30:45 +0300

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-ss-manager -k `uuidgen` -m aes-256-gcm -u --fast-open --reuse-port --manager-address /var/run/ss-manager.socks &
+nohup ss-manager -k `uuidgen` -m aes-256-gcm -u --fast-open --reuse-port --manager-address /var/run/ss-manager.socks &
 python3 worker.py
 
