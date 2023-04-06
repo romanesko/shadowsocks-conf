89d1f57ce36385fa1622b5d723828606402f0485
Roman Bykovskiy <8026787@gmail.com>
2023-04-06 19:01:11 +0300

    refactoring

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 apt update -y
 apt install git docker docker-compose -y
 cd ~
+rm -rf shadowsocks-conf
 git clone https://github.com/romanesko/shadowsocks-conf
 cd shadowsocks-conf
 docker-compose up --build
