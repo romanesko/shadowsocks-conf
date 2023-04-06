d21a810255eece038cd19ed2a928683458508a9b
Roman Bykovskiy <8026787@gmail.com>
2023-04-07 01:34:30 +0300

    refactoring

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-tmux new -d 'ss-manager -k `uuidgen` -m aes-256-gcm -u --fast-open --reuse-port --manager-address /var/run/ss-manager.socks'
-python3 worker.py
+ss-manager -k `uuidgen` -m aes-256-gcm -u --fast-open --reuse-port --manager-address /var/run/ss-manager.socks
 
